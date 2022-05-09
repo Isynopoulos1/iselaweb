9ca3656a8a3970ffbd8ca95cadf8bda7675e3756
Isela <isela.v.alarcon@gmail.com>
2022-05-09 20:48:02 +0200

    fising the about page

--- src/components/pages/about/About.js
+++ src/components/pages/about/About.js
@@ -11,22 +11,24 @@ const About = () => {
         alt="Photo"
       />
       <Description>
-        <Greetings>Hi :) </Greetings>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry . Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in the 1960s with the release
-        of Letraset sheets containing Lorem Ipsum passages, and more recently
-        with desktop publishing software like Aldus PageMaker including versions
-        of Lorem Ipsum. Contrary to popular belief, Lorem Ipsum is not simply
-        random text. It has roots in a piece of classical Latin literature from
-        45 BC, making it over 2000 years old. Richard McClintock, a Latin
-        professor at Hampden-Sydney College in Virginia, looked up one of the
-        more obscure Latin words, consectetur, from a Lorem Ipsum passage, and
-        going through the cites of the word in classical literature, discovered
-        the undoubtable source.
+        <Greetings>Hi, I'm Isela :) </Greetings>
+        I'm a UX/UI Designer from Mexico currently based in Barcelona. I've a
+        strong passion for design, art, cooking, digital tendencies, fashion,
+        cinema, architecture & animals. With a background in the digital world
+        as a Senior Art director, I've been in the advertising industry for over
+        10 years. Mostly, I've worked in top digital advertising agencies,
+        designing for brands such as Paypal, Movistar, Amazon, Nike, Purina,
+        Gerber. In 2020, I decided to move to Barcelona to attend a workshop on
+        Design Thinking in Brother Bcn. I am very proficient in After Effects,
+        Adobe suite (Photoshop, Illustrator, Premiere), and different design
+        tools and frameworks such as Figma. I enjoy building sites implementing
+        a design system. I also have a huge interest in Web development, It's
+        reflected by my knowledge in HTML, CSS, SASS, Javascript with libraries
+        such as React and Nodejs. During the last 2 years, I’ve complimented my
+        career as a digital designer with certificates, workshops, and working
+        on my own as a web developer. My profile matches such a hybrid between
+        designer and web developer. In fact, I love working in both areas
+        equally!
       </Description>
     </AboutContainer>
   );
