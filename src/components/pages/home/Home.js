2fe74a1a6cb00132802b30c98c4bbab44e87d950
Isela <isela.v.alarcon@gmail.com>
2022-05-30 04:16:52 -0500

    setting the home video

--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -1,15 +1,22 @@
 import React from "react";
 
 //IMPORT STYLES
-import { HomeContainer, HomeImg } from "./Home.styles";
+import { HomeContainer, MainVideo, Container } from "./Home.styles";
 
 const Home = () => {
   return (
     <HomeContainer>
-      <HomeImg
-        src="https://ik.imagekit.io/ppayaz/iselalarcon/ALO_erlIJahiR.png?ik-sdk-version=javascript-1.4.3&updatedAt=1652094814011"
-        alt="main img"
-      />
+      <Container>
+        <MainVideo
+          id="video"
+          src="https://ik.imagekit.io/ppayaz/iselalarcon/home/main_video_iselaweb_b90NrX4S1.mp4?ik-sdk-version=javascript-1.4.3&updatedAt=1653900920001"
+          autoPlay
+          muted
+          loop
+        >
+          Tu navegador no admite el elemento <code>video</code>.
+        </MainVideo>
+      </Container>
     </HomeContainer>
   );
 };
--- src/components/pages/home/Home.styles.js
+++ src/components/pages/home/Home.styles.js
@@ -1,16 +1,24 @@
 import styled from "styled-components";
+import { colors, sizes, fonts, margins } from "@styles";
 
 export const HomeContainer = styled.div`
   display: flex;
+  flex-direction: column;
   width: 100%;
   height: 100%;
   justify-content: center;
   align-items: center;
 `;
-export const HomeImg = styled.img`
+export const Container = styled.div`
   display: flex;
-  width: calc(100% - 100px);
-  max-width: 1200px;
-  margin-top: 120px;
-  padding: 20px 50px;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`;
+export const MainVideo = styled.video`
+  display: flex;
+  margin-top: ${margins.xs};
+  justify-content: center;
+  align-items: center;
+  width: 75%;
 `;
