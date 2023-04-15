e401f666695f2d79e4d5aca7b70c9d288234eb91
Isela Alarcón <isela.v.alarcon@gmail.com>
2023-04-15 20:10:46 +0200

    removing the behance icon in contact section

--- src/components/elements/social/Social.js
+++ src/components/elements/social/Social.js
@@ -13,7 +13,6 @@ import InstagramLogo from "@assets/icons/ig-logo";
 const Social = () => {
   return (
     <SocialContainer>
-      <BehanceLogo color="black" width="16px" height="16px" href="https://www.behance.net/iselalarcon" />
       <LinkedinLogo color="black" width="11px" href="https://www.linkedin.com/in/isela-v-alarc%C3%B3n/" />
       <GitLogo color="black" width="13px" href="https://github.com/Isynopoulos1" />
       <InstagramLogo color="black" width="16px" href="https://www.instagram.com/isynnopoulos/" />
