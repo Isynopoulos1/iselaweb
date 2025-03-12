097f081435603b81b7913927edcf035a9e54c8ba
Isela Alarcón <isela.v.alarcon@gmail.com>
2025-03-12 16:38:40 +0100

    adding medium logo to footer

--- src/components/elements/social/Social.js
+++ src/components/elements/social/Social.js
@@ -7,14 +7,14 @@ import { SocialContainer } from "./Social.styles";
 
 import LinkedinLogo from "@assets/icons/in-logo";
 import GitLogo from "@assets/icons/git-logo";
-// import InstagramLogo from "@assets/icons/ig-logo";
+import MediumLogo from "@assets/icons/medium-logo";
 
 const Social = () => {
   return (
     <SocialContainer>
       <LinkedinLogo color="black" width="11px" target="_blank" href="https://www.linkedin.com/in/isela-v-alarcon/" />
       <GitLogo color="black" width="13px" target="_blank" href="https://github.com/Isynopoulos1" />
-      {/* <InstagramLogo color="black" width="16px" target="_blank" href="https://www.instagram.com/isynnopoulos/" /> */}
+      <MediumLogo color="black" width="13px" target="_blank" href="https://medium.com/@isela.v.alarcon"/>
     </SocialContainer>
   );
 };
